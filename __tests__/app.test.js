16bce051dca233a55990cba78cb6c2e4b2748b3f
CalStott <caleb.stott@outlook.com>
2024-11-25 13:56:26 +0000

    test and complete 3-get-api-articles-article_id with error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -22,14 +22,15 @@ describe('GET /api', () => {
 				expect(endpoints).toEqual(endpointsJson);
 			});
 	});
-	test('400: Responds with an error message when passed an incorrect url', () => {
+	test('404: Responds with an error message when passed an invalid url', () => {
 		return request(app)
 			.get('/not-a-url')
-			.expect(400)
+			.expect(404)
 			.then(({ body: { msg } }) => {
-				expect(msg).toBe('Bad request');
+				expect(msg).toBe('Not found');
 			});
 	});
+
 	describe('GET /api/topics', () => {
 		test('200: Responds with an array of topic objects containing slug and description keys', () => {
 			return request(app)
@@ -45,27 +46,41 @@ describe('GET /api', () => {
 					});
 				});
 		});
-		test('400: Responds with an error message when passed an incorrect url', () => {
+	});
+
+	describe('GET /api/articles/:article_id', () => {
+		test('200: Responds with an object detailing the information for the specified article', () => {
 			return request(app)
-				.get('/api/topic')
+				.get('/api/articles/1')
+				.expect(200)
+				.then(({ body: { article } }) => {
+					expect(article).toMatchObject({
+						article_id: 1,
+						author: expect.any(String),
+						title: expect.any(String),
+						body: expect.any(String),
+						topic: expect.any(String),
+						created_at: expect.any(String),
+						votes: expect.any(Number),
+						article_img_url: expect.any(String),
+					});
+				});
+		});
+		test('404: Responds with an error message when passed a valid url parameter but there is no content there', () => {
+			return request(app)
+				.get('/api/articles/9046')
+				.expect(404)
+				.then(({ body: { msg } }) => {
+					expect(msg).toBe('Not found');
+				});
+		});
+		test('400: Responds with an error message when passed an invalid url parameter', () => {
+			return request(app)
+				.get('/api/articles/dog')
 				.expect(400)
 				.then(({ body: { msg } }) => {
 					expect(msg).toBe('Bad request');
 				});
-		});
-		test('404: Responds with an error message when passed a correct url but there is no information to return', () => {
-			return db.query(`DROP TABLE comments;`).then(() => {
-				return db.query(`DROP TABLE articles;`).then(() => {
-					return db.query(`DELETE FROM topics;`).then(() => {
-						return request(app)
-							.get('/api/topics')
-							.expect(404)
-							.then(({ body: { msg } }) => {
-								expect(msg).toBe('Not found');
-							});
-					});
-				});
-			});
 		});
 	});
 });
